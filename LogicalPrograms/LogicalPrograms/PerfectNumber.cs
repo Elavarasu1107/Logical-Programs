87e49fc75beb6b953dedb39c34597330d4d82393
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-19 18:00:50 +0530

    [Elavarasu] Perfect Number

---
+++ LogicalPrograms/LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PerfectNumber
+    {
+        public void PerfectNumberCheck()
+        {
+            Console.WriteLine("Enter a Value:");
+            int userInput = Convert.ToInt32(Console.ReadLine()), temp=0;
+
+            if(userInput % 2 == 0)
+            {
+                for(int i = 1; i < userInput;i++)
+                {
+                    if (userInput % i == 0)
+                    {
+                        temp = temp + i;
+                    }
+                }
+                if(temp == userInput)
+                {
+                    Console.WriteLine("Entered Number " + userInput + " is a Perfect Number");
+                }
+                else
+                {
+                    Console.WriteLine("Entered Number " + userInput + " is not a Perfect Number");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered Number " + userInput + " is not a Perfect Number");
+            }
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -6,8 +6,31 @@ namespace LogicalPrograms
     {
         public static void Main(String[] args)
         {
-            FibonacciSeries getMethod = new FibonacciSeries();
-            getMethod.Fibonacci();
+            Console.WriteLine("Enter a value to choose the Program");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Fibonacci Series");
+                        FibonacciSeries getMethod = new FibonacciSeries();
+                        getMethod.Fibonacci();
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Perfect Number Program");
+                        PerfectNumber getMethod = new PerfectNumber();
+                        getMethod.PerfectNumberCheck();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Enter a Number between(1-2)");
+                        break;
+                    }
+            }
         }
     }
 }
